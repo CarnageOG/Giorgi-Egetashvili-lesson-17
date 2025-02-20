5822237c7599b0a8257c354dda0e5fc8c569da27
CarnageOG <egetashviligiorgi9@gmail.com>
2025-02-20 19:17:29 +0400

    made switch

--- javaScript/app.js
+++ javaScript/app.js
@@ -20,3 +20,43 @@ do {
     console.log(j);
     j++;
 } while (j <= 25);
+
+
+
+// 4.///////////////////////////////////
+const numbers = [4, 5, 6, 7, 8, 9, 23, 24, 25, 26, 29];
+for (let num of numbers) {
+    if (num % 2 === 0) {
+        console.log(num);
+    }
+}
+
+
+
+// 5.///////////////////////////////////
+const currentDay = new Date().getDay();
+switch (currentDay) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Sunday");
+}
